28c5bc1fe0f0fabeb2c95db7443899423624cc22
jaycammarano <jaycammarano@gmail.com>
2020-12-21 13:10:37 -0500

    started landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import LandingPage from './pages/LandingPage';
 
+// TO_DO: Login status
 const App: React.FC = () => {
-  return <div>helloworld</div>;
+  return <BrowserRouter><Switch><Route path="/" exact render={() => (<LandingPage loginStatus="NOT_LOGGED_IN"/>)}/></Switch></BrowserRouter>
 };
 
 export default App;
