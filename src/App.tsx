75c6893abcb3b8359ec6e8b4be1fe0c759f088e1
jaycammarano <jaycammarano@gmail.com>
2020-12-27 08:17:33 -0500

    clean up, npm i

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import LandingPage from './pages/LandingPage';
 
 // TO_DO: Login status
 const App: React.FC = () => {
-  return <BrowserRouter><Switch><Route path="/" exact render={() => (<LandingPage loginStatus="NOT_LOGGED_IN"/>)}/></Switch></BrowserRouter>
+  return (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact render={() => (<LandingPage loginStatus="NOT_LOGGED_IN"/>)}/>
+    </Switch>
+  </BrowserRouter>
+  )
 };
 
 export default App;
