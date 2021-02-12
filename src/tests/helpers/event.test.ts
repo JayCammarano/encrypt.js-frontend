df76cef0557e4ca68fd0ed589a2156d832879db6
jaycammarano <jaycammarano@gmail.com>
2021-02-11 20:33:03 -0500

    event tests passing

--- src/tests/helpers/event.test.ts
+++ src/tests/helpers/event.test.ts
@@ -8,9 +8,15 @@ date: "string",
 location: "string",
 invitees: ["user1", "user2", "user3", "user3"],
 }
-const eventObject = new Event(body)
+
+const privateKey = "FB+p+ImF8B8NcrK8mCtBAcFaugaNo9Adl+ybMGuSDM0="
+const eventObject = new Event(body, privateKey)
 it('removes duplicate invitees', async () => {
     const uniqueInvitees = await eventObject.stripDupeInvitees()
     expect(uniqueInvitees).toStrictEqual(["user1", "user2", "user3"])
 })
 
+it('returns an encryted string', () => {
+    const encryptedEvent = eventObject.newEventPrep()
+    expect(typeof encryptedEvent).toBe("string")
+})
