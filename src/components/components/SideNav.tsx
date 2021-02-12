b36815102f571faf7b89dcd33d875972366f714f
jaycammarano <jaycammarano@gmail.com>
2021-02-11 22:45:10 -0500

    rows!

--- src/components/components/SideNav.tsx
+++ src/components/components/SideNav.tsx
@@ -7,9 +7,9 @@ const SideNav: React.FC<IEventViews> = ({setWhichTab}) => {
     }
 
     return (
-        <div className="w-4/12 border-2 border-black ">
-            <div className="p-4 border-b-2 border-black" onClick={()=> setTab("myEvents")}>My Events</div>
-            <div className="p-4 border-b-2 border-black" onClick={()=> setTab("events")}>Events</div>
+        <div className="flex flex-row w-4/12 border-2 border-black ">
+            <div className="p-4 border-black" onClick={()=> setTab("myEvents")}>My Events</div>
+            <div className="p-4 border-black" onClick={()=> setTab("events")}>Events</div>
             <div className="p-4" onClick={()=> setTab("inbox")}>Inbox</div>
         </div>
     )
