52026d571a82fe15dabcc8c8132cca4cb9a1b885
jaycammarano <jaycammarano@gmail.com>
2021-03-26 22:32:02 -0400

    event detail layout

--- src/components/components/views/MyEventDetails.tsx
+++ src/components/components/views/MyEventDetails.tsx
@@ -3,8 +3,9 @@ import { IEventDetails } from '../../../helpers/eventsInterface'
 
 const MyEventDetails: React.FC<IEventDetails> = ({event}) => {
     return (
-        <div>
-            <p>{event.title} - {event.date}</p>
+        <div className="m-4">
+            <h5 className="p-4 text-2xl font-medium leading-tight text-gray-900 title-font sm:text-4xl">{event.title}</h5>
+            <p className="text-xs text-gray-600">{event.date}</p>
             <p>{event.description}</p>
             <p>{event.location}</p>
         </div>
