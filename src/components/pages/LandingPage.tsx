9c6c7b9d92259bf188367e426de5d5638c7a88f4
jaycammarano <jaycammarano@gmail.com>
2021-03-31 14:12:29 -0400

    resize feature examples

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -47,8 +47,8 @@ const LandingPage: React.FC = () => {
           <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2"></div>
         </div>
       </section>
-      <section>
-          <div className="flex flex-col items-center ml-12 text-center b-12 md:mb-16 lg:flex-grow md:w-1/2 md:items-start md:text-left">
+      <section className="h-screen">
+          <div className="flex flex-col items-center mb-4 ml-12 text-center b-12 md:mb-16 lg:flex-grow md:w-1/2 md:items-start md:text-left">
             <div><h1 className="mb-4 text-3xl font-medium leading-tight text-gray-900 title-font sm:text-4xl">
               Organize Events With Ease
             </h1>
@@ -66,9 +66,7 @@ const LandingPage: React.FC = () => {
               />
           </div>
           </div>
-      </section>
-      <section>
-        <div className="flex flex-col mr-12 text-right b-12 md:mb-16 lg:flex-grow md:items-end md:text-end">
+        <div className="flex flex-col mt-4 mr-12 text-right b-12 md:mb-16 lg:flex-grow md:items-end md:text-end">
         <div className="float-left w-1/2 lg:max-w-lg lg:w-full md:w-1/2">
           <h1 className="mb-4 text-3xl font-medium leading-tight text-gray-900 title-font sm:text-4xl">
             Keep Track of Upcoming Events
@@ -91,8 +89,7 @@ const LandingPage: React.FC = () => {
 
       <section className="text-gray-700 body-font">
       <div className="container flex flex-col items-center px-4 py-6 mx-auto border-2 border-black rounded sm:py-12 md:py-24 md:flex-row">
-          <div className="flex flex-col items-center mb-12 ml-4 text-center lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
-            
+          <div className="flex flex-col items-center mb-12 ml-4 text-center lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0"> 
             <h1 className="mb-4 text-3xl font-medium leading-tight text-gray-900 title-font sm:text-4xl">
             How it works:
             </h1>
@@ -117,11 +114,11 @@ const LandingPage: React.FC = () => {
 
               <p className="mb-8 leading-relaxed">
                 Symmetrical encryption system that uses only a private key and a nonce to encrypt a message. A nonce is a one-time use random string that is unique to each message.</p>
-                <ol> <li>• User A encrypts the message with their private key and a nonce. </li>
-                <li>• They then decrypt the message with the nonce and the private key</li></ol><br/>
-              
-
-
+                <ol> 
+                  <li>• User A encrypts the message with their private key and a nonce. </li>
+                  <li>• They then decrypt the message with the nonce and the private key</li>
+                </ol>
+                <br/>
               <h5 className="mb-4 text-xl font-medium leading-tight text-gray-900 title-font sm:text-xl">
               Implementation:</h5>
               <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
@@ -131,9 +128,7 @@ const LandingPage: React.FC = () => {
                   src=""
                 />
               </div>
-
               <p className="mb-8 leading-relaxed">
-
               On sign up the backend generates a private key for the user. This is encoded to a Base64 string to store in the database. This is done on the backend to prevent man in the middle attacks from accessing unencrypted keys.
               When the user creates an event the client encrypts the event using their private key and unique nonce. This is encoded to Base64 with the nonce.
               Slide: decryption and encryption. Then when an invite recipient logs in the backend decodes the event and reencodes it with the recipient’s private key and a newly generated nonce. 
