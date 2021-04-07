5d9a48a8028fd37ec01b9d3461f56484829ebdcb
jaycammarano <jaycammarano@gmail.com>
2021-04-07 16:30:42 -0400

    landing pageupdated

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -49,46 +49,6 @@ const LandingPage: React.FC = () => {
           <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2"></div>
         </div>
       </section>
-      <section className="h-screen">
-          <div className="flex flex-col items-center mb-4 ml-12 text-center b-12 md:mb-16 lg:flex-grow md:w-1/2 md:items-start md:text-left">
-            <div><h1 className="mb-4 text-3xl font-medium leading-tight text-gray-900 title-font sm:text-4xl">
-              Organize Events With Ease
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              Maybe we should do borders around each feature instead of the
-              whole walkthrough being one border. This image could be of just
-              this one feature.
-            </p>
-            </div>
-          <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
-            <img
-              className="object-cover object-center w-full h-full rounded shadow"
-              alt="spacefiller"
-              src=""
-              />
-          </div>
-          </div>
-        <div className="flex flex-col mt-4 mr-12 text-right b-12 md:mb-16 lg:flex-grow md:items-end md:text-end">
-        <div className="float-left w-1/2 lg:max-w-lg lg:w-full md:w-1/2">
-          <h1 className="mb-4 text-3xl font-medium leading-tight text-gray-900 title-font sm:text-4xl">
-            Keep Track of Upcoming Events
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            Maybe we should do borders around each feature instead of the
-            whole walkthrough being one border. This image could be of just
-            this one feature.
-          </p>
-          </div>
-          <div className="float-right w-1/2 lg:max-w-lg lg:w-full md:w-1/2">
-            <img
-              className="object-cover object-center w-full h-full rounded shadow"
-              alt="spacefiller"
-              src=""
-            />
-        </div>
-        </div>           
-      </section>
-
       <section className="text-gray-700 body-font">
       <div className="container flex flex-col items-center px-4 py-6 mx-auto border-2 border-black rounded sm:py-12 md:py-24 md:flex-row">
           <div className="flex flex-col items-center mb-12 ml-4 text-center lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0"> 
@@ -101,13 +61,6 @@ const LandingPage: React.FC = () => {
             <h4 className="mb-4 text-2xl font-medium leading-tight text-gray-900 title-font sm:text-2xl">
               The Specifics:
             </h4>            
-              <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
-              <img
-                  className="object-cover object-center w-full h-full rounded shadow"
-                  alt="libsodium"
-                  src=""
-                />
-             </div>
             <p className="mb-8 leading-relaxed">
               The project takes advantage of the open-source, audited libsodium encryption library. It uses the symmetric “Secret Box” method.
             </p>
@@ -123,31 +76,14 @@ const LandingPage: React.FC = () => {
                 <br/>
               <h5 className="mb-4 text-xl font-medium leading-tight text-gray-900 title-font sm:text-xl">
               Implementation:</h5>
-              <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
-                <img
-                  className="object-cover object-center w-full h-full rounded shadow"
-                  alt="tweetncal"
-                  src=""
-                />
-              </div>
               <p className="mb-8 leading-relaxed">
               On sign up the backend generates a private key for the user. This is encoded to a Base64 string to store in the database. This is done on the backend to prevent man in the middle attacks from accessing unencrypted keys.
               When the user creates an event the client encrypts the event using their private key and unique nonce. This is encoded to Base64 with the nonce.
               Slide: decryption and encryption. Then when an invite recipient logs in the backend decodes the event and reencodes it with the recipient’s private key and a newly generated nonce. 
               </p>
           </div>
-          <div className="object-cover object-center w-5/6 rounded shadow lg:max-w-lg lg:w-full md:w-1/2">
-            <img
-              className="object-cover object-center w-full h-full rounded shadow"
-              alt="space filler"
-              src=""
-            />
-          </div>
         </div>
-        <div className="container flex flex-col items-center px-4 mx-auto my-6 sm:py-12 md:py-24 md:flex-row">
-          </div>
-
-          </section>
+      </section>
 
         </Fragment>
     )
